e11caed7f4adbd49cf5a63b3e7314b71bad0f0e5
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2024-02-09 19:00:45 +0000

    changed javascript to use import to match package.json

--- generateReadMe.js
+++ generateReadMe.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const readline = require('readline');
+import { createInterface } from 'readline';
+import fs from 'fs';
 
-function writeToFile(data) {
+const writeToFile = (data) => {
   fs.writeFile('README.md', data, (err) => {
     if (err) {
       console.error('Error writing to file:', err);
@@ -9,38 +9,37 @@ function writeToFile(data) {
       console.log('README file has been successfully created!');
     }
   });
-}
+};
 
-function gatherUserInput() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+const questions = [
+  { type: 'input', name: 'title', message: 'What is your project title?' },
+  { type: 'input', name: 'description', message: 'Please provide a description of your project:' },
+  { type: 'input', name: 'installation', message: 'What are the installation instructions?' },
+  { type: 'input', name: 'usage', message: 'How do you use this project?' },
+  { type: 'input', name: 'contributing', message: 'How can others contribute to this project?' },
+  { type: 'input', name: 'tests', message: 'What tests have been run on this project?' },
+  { type: 'input', name: 'github', message: 'What is your GitHub username?' },
+  { type: 'input', name: 'email', message: 'What is your email address?' },
+];
 
-  const questions = [
-    { type: 'input', name: 'title', message: 'What is your project title?' },
-    { type: 'input', name: 'description', message: 'Please provide a description of your project:' },
-    { type: 'input', name: 'installation', message: 'What are the installation instructions?' },
-    { type: 'input', name: 'usage', message: 'How do you use this project?' },
-    { type: 'input', name: 'contributing', message: 'How can others contribute to this project?' },
-    { type: 'input', name: 'tests', message: 'What tests have been run on this project?' },
-    { type: 'input', name: 'github', message: 'What is your GitHub username?' },
-    { type: 'input', name: 'email', message: 'What is your email address?' },
-  ];
+const answers = {};
 
-  const answers = {};
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-  function askQuestion(index) {
-    const question = questions[index];
+function askQuestion(index) {
+  const question = questions[index];
 
-    rl.question(question.message, (input) => {
-      answers[question.name] = input;
+  rl.question(question.message, (input) => {
+    answers[question.name] = input;
 
-      if (index < questions.length - 1) {
-        askQuestion(index + 1);
-      } else {
-        rl.close();
-        const readmeTemplate = `
+    if (index < questions.length - 1) {
+      askQuestion(index + 1);
+    } else {
+      rl.close();
+      const readmeTemplate = `
   # ${answers.title}
   
   ## Description
@@ -65,12 +64,9 @@ function gatherUserInput() {
   - Email: ${answers.email}
   `;
 
-        writeToFile(readmeTemplate);
-      }
-    });
-  }
-
-  askQuestion(0);
+      writeToFile(readmeTemplate);
+    }
+  });
 }
 
-gatherUserInput();
+askQuestion(0);
