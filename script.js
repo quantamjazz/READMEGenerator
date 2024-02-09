c989bf03010b708a63056a5b45cb10f19d99998b
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2024-02-09 16:19:43 +0000

    added some questions for user to input answers to generate their readme file

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const questions = [
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'What is the title of your project?',
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project',
+      },
+
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install this application?',
+      },
+
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Explain how to use your application',
+      },
+
+      {
+        type: 'input',
+        name: 'licence',
+        message: 'Detail which licence your application uses',
+      },
+    
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can developers or users contribute to your application?',
+      },
+
+  ];
+  
