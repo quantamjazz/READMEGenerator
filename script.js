eee0b7e0bcc84eca47f02dfb4e65af74c6308cc9
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2024-02-09 18:22:50 +0000

    added generateReadMe.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const inquirer = require('inquirer');
+import inquirer from 'inquirer';
 const fs = require('fs');
 
 const questions = [
@@ -40,3 +40,30 @@ const questions = [
 
   ];
   
+  inquirer.prompt(questions)
+  .then((answers) => {
+    // Use the answers to dynamically generate the README
+    generateREADME(answers);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+  function generateREADME(answers) {
+    // Read the template file
+    const template = fs.readFileSync('template.md', 'utf8');
+  
+    // Replace placeholders in the template with user input
+    const generatedREADME = template
+      .replace('<!-- Describe what your project does -->', answers.projectTitle)
+      // Add similar lines for other sections
+  
+    // Write the generated README to a file
+    fs.writeFileSync('README.md', generatedREADME);
+  
+    console.log('README.md generated successfully!');
+  }
+
+  // Call the function to start the README generation process
+generateREADME();
+
