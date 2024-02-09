37304d59b8cb39956ba27331431b676b138a5da3
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2024-02-09 19:32:37 +0000

    edited code to replace accidental deletion of inquier and import code

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# READMEGenerator
+
+# reset
+  
+## Description
+
+  wehje
+  
+## Installation
+
+  rj
+  
+## Usage
+
+  rjer
+  
+## Contributing
+
+  erj
+  
+## Tests
+
+  rgt
+  
+## Questions
+
+  If you have any questions, please reach out to me at:
+  
+- GitHub: [rt](https://github.com/rt)
+- Email: ryhr
+  
--- generateReadMe.js
+++ generateReadMe.js
@@ -1,59 +1,109 @@
+const inquirer = require('inquirer');
+ readline = require('readline');
+
+const questions = [
+  {
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    name: 'description',
+    message: 'Please provide a description of your project.'
+  },
+  {
+    name: 'installation',
+    message: 'Please provide instructions for installing your project.'
+  },
+  {
+    name: 'usage',
+    message: 'Please provide instructions for using your project.'
+  },
+  {
+    name: 'contributing',
+    message: 'Please provide instructions for contributing to your project.'
+  },
+  {
+    name: 'tests',
+    message: 'Please provide instructions for testing your project.'
+  },
+  {
+    name: 'github',
+    message: 'What is your GitHub username?'
+  },
+  {
+    name: 'email',
+    message: 'What is your email address?'
+  }
+];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 function askQuestion(index) {
-    const question = questions[index];
-  
-    rl.question(question.message, (input) => {
-      answers[question.name] = input;
-  
-      if (index < questions.length - 1) {
-        askQuestion(index + 1);
-      } else {
-        rl.close();
-        let readmeTemplate = `
+  const question = questions[index];
+
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: question.name,
+      message: question.message
+    }
+  ]).then((answers) => {
+    if (index < questions.length - 1) {
+      askQuestion(index + 1);
+    } else {
+      rl.close();
+      let readmeTemplate = `
     # ${answers.title}
     `;
-  
-    // Add Table of Contents
-    readmeTemplate += `
-    ## Table of Contents
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
-    `;
-  
-    // Add sections with anchors
-    readmeTemplate += `
-    ## Description
-    <a name="description"></a>
-    ${answers.description}
-  
-    ## Installation
-    <a name="installation"></a>
-    ${answers.installation}
-  
-    ## Usage
-    <a name="usage"></a>
-    ${answers.usage}
-  
-    ## Contributing
-    <a name="contributing"></a>
-    ${answers.contributing}
-  
-    ## Tests
-    <a name="tests"></a>
-    ${answers.tests}
-  
-    ## Questions
-    <a name="questions"></a>
-    If you have any questions, please reach out to me at:
-  
-    - GitHub: [${answers.github}](https://github.com/${answers.github})
-    - Email: ${answers.email}
-    `;
-  
-        writeToFile(readmeTemplate);
-      }
-    });
-  }
+      // Table of Contents
+      readmeTemplate += `
+      ## Table of Contents
+      - [Description](#description)
+      - [Installation](#installation)
+      - [Usage](#usage)
+      - [Contributing](#contributing)
+      - [Tests](#tests)
+      - [Questions](#questions)
+      `;
+      // anchors
+      readmeTemplate += `
+      ## Description
+      <a name="description"></a>
+      ${answers.description}
+
+      ## Installation
+      <a name="installation"></a>
+      ${answers.installation}
+
+      ## Usage
+      <a name="usage"></a>
+      ${answers.usage}
+
+      ## Contributing
+      <a name="contributing"></a>
+      ${answers.contributing}
+
+      ## Tests
+      <a name="tests"></a>
+      ${answers.tests}
+
+      ## Questions
+      <a name="questions"></a>
+      If you have any questions, please reach out to me at:
+
+      - GitHub: [${answers.github}](https://github.com/${answers.github})
+      - Email: ${answers.email}
+      `;
+      writeToFile(readmeTemplate);
+    }
+  });
+}
+
+askQuestion(0);
+
+function writeToFile(readmeTemplate) {
+  // code to write the `readmeTemplate` string to a file
+}
