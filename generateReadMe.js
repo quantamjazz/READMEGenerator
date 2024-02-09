7bcd3f91db7bd4c4f187d5c7843522c47a20e19e
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2024-02-09 18:57:19 +0000

    simplified javascript code, deleted extra script file

--- generateReadMe.js
+++ generateReadMe.js
@@ -2,54 +2,45 @@ const fs = require('fs');
 const readline = require('readline');
 
 function writeToFile(data) {
-    fs.writeFile('README.md', data, (err) => {
-      if (err) {
-        console.error('Error writing to file:', err);
-      } else {
-        console.log('README file has been successfully created!');
-      }
-    });
-  }
-
-  function gatherUserInput() {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-  
-    const questions = [
-      { type: 'input', name: 'title', message: 'What is your project title?' },
-      { type: 'input', name: 'description', message: 'Please provide a description of your project:' },
-      { type: 'input', name: 'installation', message: 'What are the installation instructions?' },
-      { type: 'input', name: 'usage', message: 'How do you use this project?' },
-      { type: 'input', name: 'contributing', message: 'How can others contribute to this project?' },
-      { type: 'input', name: 'tests', message: 'What tests have been run on this project?' },
-      { type: 'input', name: 'github', message: 'What is your GitHub username?' },
-      { type: 'input', name: 'email', message: 'What is your email address?' },
-    ];
-  
-    const answers = {};
-  
-    function askQuestion(index) {
-      const question = questions[index];
-  
-      rl.question(question.message, (input) => {
-        answers[question.name] = input;
-  
-        if (index < questions.length - 1) {
-          askQuestion(index + 1);
-        } else {
-          rl.close();
-          generateREADME(answers);
-        }
-      });
+  fs.writeFile('README.md', data, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log('README file has been successfully created!');
     }
-  
-    askQuestion(0);
-  }
-  
-  function generateREADME(answers) {
-    const readmeTemplate = `
+  });
+}
+
+function gatherUserInput() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const questions = [
+    { type: 'input', name: 'title', message: 'What is your project title?' },
+    { type: 'input', name: 'description', message: 'Please provide a description of your project:' },
+    { type: 'input', name: 'installation', message: 'What are the installation instructions?' },
+    { type: 'input', name: 'usage', message: 'How do you use this project?' },
+    { type: 'input', name: 'contributing', message: 'How can others contribute to this project?' },
+    { type: 'input', name: 'tests', message: 'What tests have been run on this project?' },
+    { type: 'input', name: 'github', message: 'What is your GitHub username?' },
+    { type: 'input', name: 'email', message: 'What is your email address?' },
+  ];
+
+  const answers = {};
+
+  function askQuestion(index) {
+    const question = questions[index];
+
+    rl.question(question.message, (input) => {
+      answers[question.name] = input;
+
+      if (index < questions.length - 1) {
+        askQuestion(index + 1);
+      } else {
+        rl.close();
+        const readmeTemplate = `
   # ${answers.title}
   
   ## Description
@@ -73,8 +64,13 @@ function writeToFile(data) {
   - GitHub: [${answers.github}](https://github.com/${answers.github})
   - Email: ${answers.email}
   `;
-  
-    writeToFile(readmeTemplate);
+
+        writeToFile(readmeTemplate);
+      }
+    });
   }
 
-  
+  askQuestion(0);
+}
+
+gatherUserInput();
